66d631487a234d6bf258a71302f00a3d819190e0
anol123 <anol.roy123@gmail.com>
2026-02-22 15:45:08 +0530

    added various annotations mapping

--- src/main/java/com/example/springBootLearning_1/controller/UserController.java
+++ src/main/java/com/example/springBootLearning_1/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.springBootLearning_1.controller;
 
+import com.example.springBootLearning_1.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,11 +20,28 @@ public class UserController {
     public String greetUser(@RequestParam(defaultValue = "Guest") String name){
         return "Hello"+name;
     }
+    @GetMapping("/greet/{id}")
+    public String greetUser(@PathVariable int id){
+        return "Hello User "+id;
+    }
+    @GetMapping("/greet/json")
+    public User getUserJson(){
+        return new User(1, "Anol",27);
+    }
 
     @PostMapping
     public String test2(){
         return "Post method is hit";
     }
 
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user){
+        return user;
+    }
+    @PostMapping("/createHttp")
+    public ResponseEntity<User> createUserHttp(@RequestBody User user){
+        //return ResponseEntity.ok(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 
 }
